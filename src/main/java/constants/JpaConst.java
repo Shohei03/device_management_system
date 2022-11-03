b7c2ea6721583370306b327cc458a01d6ecf2963
Shohei Matoba <sn13724@gmail.com>
2022-11-04 05:55:50 +0900

    定数ファイルを作成

---
+++ src/main/java/constants/JpaConst.java
@@ -0,0 +1,59 @@
+package constants;
+
+/**
+ *
+ * DB関連の項目値を定義するインターフェース
+ *
+ */
+
+public interface JpaConst {
+
+    //persistence-unit名
+    String PERSISTENCE_UNIT_NAME = "device_management_system";
+
+    //データ取得件数の最大値
+    int ROW_PER_PAGE = 20; //1ページに表示するレコードの数
+
+    //従業員テーブル
+    String TABLE_EMP = "employees"; //テーブル名
+    //従業員テーブルカラム
+    String EMP_COL_ID = "id"; //id
+    String EMP_COL_CODE = "code"; //社員番号
+    String EMP_COL_NAME = "name"; //氏名
+    String EMP_COL_PASS = "password"; //パスワード
+    String EMP_COL_ADMIN_FLAG = "admin_flag"; //管理者権限
+    String EMP_COL_CREATED_AT = "created_at"; //登録日時
+    String EMP_COL_UPDATED_AT = "updated_at"; //更新日時
+    String EMP_COL_DELETE_FLAG = "delete_flag"; //削除フラグ
+
+    int ROLE_ADMIN = 2;  //管理者権限ON(管理者)
+    int ROLE_PARTIAL_ADMIN = 1;  //管理者権限ON(デバイスデータの登録削除はできるが、従業員の登録削除まではできない）
+    int ROLE_GENERAL = 0; //管理者権限OFF(一般)
+    int EMP_DEL_TRUE = 1; //削除フラグON(削除済み)
+    int EMP_DEL_FALSE = 0; //削除フラグOFF(現役)
+
+
+
+    //Entity名
+    String ENTITY_EMP = "employee"; //従業員
+
+    //JPQL内パラメータ
+    String JPQL_PARM_CODE = "code"; //社員番号
+    String JPQL_PARM_PASSWORD = "password"; //パスワード
+    String JPQL_PARM_EMPLOYEE = "employee"; //従業員
+
+    //NamedQueryの nameとquery
+    //全ての従業員をidの降順に取得する
+    String Q_EMP_GET_ALL = ENTITY_EMP + ".getAll"; //name
+    String Q_EMP_GET_ALL_DEF = "SELECT e FROM Employee AS e ORDER BY e.id DESC"; //query
+    //全ての従業員の件数を取得する
+    String Q_EMP_COUNT = ENTITY_EMP + ".count";
+    String Q_EMP_COUNT_DEF = "SELECT COUNT(e) FROM Employee AS e";
+    //社員番号とハッシュ化済パスワードを条件に未削除の従業員を取得する
+    String Q_EMP_GET_BY_CODE_AND_PASS = ENTITY_EMP + ".getByCodeAndPass";
+    String Q_EMP_GET_BY_CODE_AND_PASS_DEF = "SELECT e FROM Employee AS e WHERE e.deleteFlag = 0 AND e.code = :" + JPQL_PARM_CODE + " AND e.password = :" + JPQL_PARM_PASSWORD;
+    //指定した社員番号を保持する従業員の件数を取得する
+    String Q_EMP_COUNT_REGISTERED_BY_CODE = ENTITY_EMP + ".countRegisteredByCode";
+    String Q_EMP_COUNT_REGISTERED_BY_CODE_DEF = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :" + JPQL_PARM_CODE;
+
+}
