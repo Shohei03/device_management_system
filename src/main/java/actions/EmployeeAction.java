f33d0b482297ba778859b1f2ca029e360fa7074f
Shohei Matoba <sn13724@gmail.com>
2022-11-05 07:47:57 +0900

    従業員newアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -19,7 +19,7 @@ public class EmployeeAction extends ActionBase {
      * メソッドを実行する
      */
     @Override
-    public void process() throws ServletException, IOException{
+    public void process() throws ServletException, IOException {
         service = new EmployeeService();
 
         //メソッドを実行
@@ -33,23 +33,23 @@ public class EmployeeAction extends ActionBase {
      * @throws ServletException
      * @throws IOException
      */
-    public void index() throws ServletException, IOException{
+    public void index() throws ServletException, IOException {
 
-      //指定されたページ数の一覧画面に表示するデータを取得
+        //指定されたページ数の一覧画面に表示するデータを取得
         int page = getPage();
         List<EmployeeView> employees = service.getPerPage(page);
 
-      //全ての従業員データの件数を取得
+        //全ての従業員データの件数を取得
         long employeeCount = service.countAll();
 
-        putRequestScope(AttributeConst.EMPLOYEES, employees);  //取得した従業員データ
-        putRequestScope(AttributeConst.EMP_COUNT, employeeCount);  //全ての従業員データの件数
-        putRequestScope(AttributeConst.PAGE, page);  //ページ数
-        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE);  ////1ページに表示するレコードの数
+        putRequestScope(AttributeConst.EMPLOYEES, employees); //取得した従業員データ
+        putRequestScope(AttributeConst.EMP_COUNT, employeeCount); //全ての従業員データの件数
+        putRequestScope(AttributeConst.PAGE, page); //ページ数
+        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE); ////1ページに表示するレコードの数
 
-      //セッションにフラッシュメッセージが設定されている場合はリクエストスコープに移し替え、セッションからは削除する
+        //セッションにフラッシュメッセージが設定されている場合はリクエストスコープに移し替え、セッションからは削除する
         String flush = getSessionScope(AttributeConst.FLUSH);
-        if(flush != null) {
+        if (flush != null) {
             putRequestScope(AttributeConst.FLUSH, flush);
             removeSessionScope(AttributeConst.FLUSH);
         }
@@ -58,7 +58,17 @@ public class EmployeeAction extends ActionBase {
         forward(ForwardConst.FW_EMP_INDEX);
     }
 
+    /**
+     * 新規登録画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void entryNew() throws ServletException, IOException{
+        putRequestScope(AttributeConst.TOKEN, getTokenId());  //CSRF対策用トークン
+        putRequestScope(AttributeConst.EMPLOYEE, new EmployeeView());  //空の従業員インスタンス
 
-
+      //新規登録画面を表示
+        forward(ForwardConst.FW_EMP_NEW);
+    }
 
 }
